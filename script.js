7d913dee2934d9eed96da18f6d69364c78b4ed69
yovita aderia <yovitaaderiasiallagan26@gmail.com>
2024-03-29 20:54:46 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("iyaButton").addEventListener("click", function () {
+  window.location.href = "teknisi.html";
+});
+
+document.getElementById("tidakButton").addEventListener("click", function () {
+  window.location.href = "mahasiswa.html";
+});
